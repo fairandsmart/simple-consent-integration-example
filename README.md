3e18ed3cb144f238bdc384387f0aa08c1a82f17d
Nicolas Rueff <nicolas.rueff@fairandsmart.com>
2020-11-13 17:07:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# simple consent integration example
+
+Some code to demonstrate how to integrate a consent form.
+
+## in a nutshell
+* adapt values in config.ini ;
+* build your image : `docker build --tag simple-consent-integration-example .`
+* run your image : `docker run simple-consent-integration-example`
+
+## configuration
+Configuration can either be done in config.ini or using environment variables
+(especially useful when running using docker).
+
+| parameter  | environment variable name  | config file key  | 
+|---|---|---|
+| auth server url | AUTH_URL | auth_url |
+| auth server realm | AUTH_REALM | auth_realm |
+| auth server clientid | AUTH_CLIENT_ID | auth_client_id |
+| auth server username | AUTH_USERNAME | auth_username |
+| auth server password | AUTH_PASSWORD | auth_password |
+| api server url | API_URL | api_url |
+| organisation id | ORGANISATION_ID | organisation_id |
+| model id or alias | MODEL_ID_OR_ALIAS | model_id_or_alias |
+
+Config file is loaded from CONFIG_FILE_PATH (default : "config.ini"). 
+ 
+## run example
+Under docker, using a specific config file "my-config.ini" :
+`docker run --mount type=bind,source=$PWD/my-config.ini,target=/var/www/html/my-config.ini -e CONFIG_FILE_PATH="my-config.ini" simple-consent-integration-example`
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+function get_conf()
+{
+    $config = parse_ini_file(key_exists("CONFIG_FILE_PATH", $_ENV) ? $_ENV["CONFIG_FILE_PATH"] : "config.ini");
+    $config["auth_url"] = key_exists("AUTH_URL", $_ENV) ? $_ENV["AUTH_URL"] : $config["auth_url"];
+    $config["auth_realm"] = key_exists("AUTH_REALM", $_ENV) ? $_ENV["AUTH_REALM"] : $config["auth_realm"];
+    $config["auth_client_id"] = key_exists("AUTH_CLIENT_ID", $_ENV) ? $_ENV["AUTH_CLIENT_ID"] : $config["auth_client_id"];
+    $config["auth_username"] = key_exists("AUTH_USERNAME", $_ENV) ? $_ENV["AUTH_USERNAME"] : $config["auth_username"];
+    $config["auth_password"] = key_exists("AUTH_PASSWORD", $_ENV) ? $_ENV["AUTH_PASSWORD"] : $config["auth_password"];
+    $config["api_url"] = key_exists("API_URL", $_ENV) ? $_ENV["API_URL"] : $config["api_url"];
+    $config["organisation_id"] = key_exists("ORGANISATION_ID", $_ENV) ? $_ENV["ORGANISATION_ID"] : $config["organisation_id"];
+    $config["model_id_or_alias"] = key_exists("MODEL_ID_OR_ALIAS", $_ENV) ? $_ENV["MODEL_ID_OR_ALIAS"] : $config["model_id_or_alias"];
+    return $config;
+}
+
+function getToken()
+{
+    $config = get_conf();
+    $auth_url = $config["auth_url"];
+    $auth_realm = $config["auth_realm"];
+    $auth_client_id = $config["auth_client_id"];
+    $auth_username = $config["auth_username"];
+    $auth_password = $config["auth_password"];
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $auth_url . "/realms/" . $auth_realm . "/protocol/openid-connect/token");
+    curl_setopt($curl, CURLOPT_POSTFIELDS, "grant_type=password&client_id=" . $auth_client_id . "&username=" . urlencode($auth_username) . "&password=" . urlencode($auth_password));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_errno($curl) > 0 && error_log("while getting token : " . curl_error($curl));
+    curl_close($curl);
+
+    return json_decode($response)->access_token;
+}
+
+function getFormUrl()
+{
+    $uuid = array_key_exists("uuid", $_GET) ? $_GET["uuid"] : uniqid();
+    $email = array_key_exists("email", $_GET) ? $_GET["email"] : "nobody@exemple.com";
+    $token = getToken();
+
+    $config = get_conf();
+    $api_url = $config["api_url"];
+    $organisation_id = $config["organisation_id"];
+    $model_id_or_alias = $config["model_id_or_alias"];
+
+    $host = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $_SERVER['HTTP_HOST'];
+    $port = isset($_SERVER['HTTP_X_FORWARDED_PORT']) ? $_SERVER['HTTP_X_FORWARDED_PORT'] : "";
+    $proto = isset($_SERVER['HTTPS']) ? "https" : "http";
+    $proto = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : $proto;
+    $me = $proto . "://" . $host . ($port ? ":" . $port : "") . $_SERVER['DOCUMENT_URI'];
+
+    $context = [
+        "userid" => $uuid,
+        "callback" => "?uuid=" . $uuid . "&email=" . $email,
+        "country" => "FR",
+        "language" => "fr",
+        "optoutEmail" => $email,
+        "receipt" => false,
+        "iframe" => true,
+        "iframeEventsTargetOrigin" => $me,
+        "optoutEmailLink" => $me . "?uuid=" . $uuid . "&email=" . $email,
+    ];
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $api_url . "/organisations/" . $organisation_id . "/consents/" . $model_id_or_alias . "/endpoint");
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($context));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token", "Content-Type: application/json"));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_errno($curl) > 0 && error_log("while getting form : " . curl_error($curl));
+    curl_close($curl);
+
+    return json_decode($response)->endpoint;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Consent iFrame Integration Test</title>
+</head>
+<body>
+<h2 style="text-align: center">Consent iFrame Integration Test</h2>
+<iframe src="<?php echo getFormUrl() ?>" width="100%" title="Consent iFrame Integration Test" id="consent"
+        name="consent"></iframe>
+<script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.0.4/iframeResizer.js"></script>
+<script type="text/javascript">iFrameResize({log: false});</script>
+<script type="application/javascript">
+    window.addEventListener("message", messageListener, false);
+
+    function messageListener(event) {
+        if (event.data.search(/consent-callback/) >= 0) {
+            const urlback = event.data.replace(/.*consent-callback\/([^"]*).*/, '$1');
+            window.location.assign(urlback);
+        }
+    }
+</script>
+</body>
+</html>
